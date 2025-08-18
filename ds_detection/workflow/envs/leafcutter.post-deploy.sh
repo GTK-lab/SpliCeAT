4aa4ed6e6b5752547257a45006428c2ee5d581d4
Greg Tucker-Kellogg <dbsgtk@nus.edu.sg>
2025-08-18 19:28:02 +0800

    Update leafcutter.post-deploy.sh
    Added more explicit install instructions to ensure CRAN repository available

--- ds_detection/workflow/envs/leafcutter.post-deploy.sh
+++ ds_detection/workflow/envs/leafcutter.post-deploy.sh
@@ -11,8 +11,9 @@ unset R_LIBS_USER
 conda env config vars unset R_LIBS_USER
 
 R --no-environ -s <<EOF
-
+    options(repos = c(CRAN = "http://cran.us.r-project.org"))
     Sys.setenv(TAR = '/bin/tar')
+    if (!("remotes" %in% rownames(installed.packages()))) install.packages("remotes")
     if (!("oompaBase" %in% rownames(installed.packages()))) install.packages("oompaBase")
     if (!("TailRank" %in% rownames(installed.packages()))) install.packages("TailRank")
     remotes::install_github("davidaknowles/leafcutter/leafcutter",ref='psi_2019',dependencies=FALSE)
